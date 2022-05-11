d8b0bb2a0c2632921a7ae20562b0462792a11332
Aarathy14 <aarathyganesh14@gmail.com>
2022-05-11 15:43:36 +0530

    function

---
+++ Review/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Paragraph from './Paragraph';
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Paragraph/>
+      </div>
+    )
+  }
+}
+
+export default App
