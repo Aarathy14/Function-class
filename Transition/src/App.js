9e671f51255869429c2daad0b64bc112cc60c0e5
Aarathy14 <aarathyganesh14@gmail.com>
2022-05-13 15:30:28 +0530

    function

---
+++ Transition/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Time from './Time.js';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Time/>
+
+      </div>
+    )
+  }
+}
+
+export default App
